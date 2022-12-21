6839f803d6dfd0dec5c26e6e1ad372030b6744de
Enock <enock.mokua@student.moringaschool.com>
2022-12-21 14:08:02 +0300

    add show and create action

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -1,6 +1,15 @@
 class NumbersController < ApplicationController
-
   def index
     render json: Number.all
+  end
+
+  def show
+    num = Number.find(params[:id])
+    render json: num
+  end
+
+  def create
+    num = Number.create(num_params)
+    json render: num
   end
 end
