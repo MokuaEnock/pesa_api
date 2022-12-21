e7a292d12e0ce25f70069468c8967b6bef3dd653
Enock <enock.mokua@student.moringaschool.com>
2022-12-21 13:35:32 +0300

    add functionality to pay_mpesa method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  require "uri"
+  require "net/http"
+
   def index
     render json: User.all
   end
@@ -19,7 +22,32 @@ class UsersController < ApplicationController
     params.permit(:email, :username, :total, :phone)
   end
 
-  def pay_mpesa
-    
+  def pay_mpesa(amount, code, phone)
+    url = URI("https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest")
+    ​
+    https = Net::HTTP.new(url.host, url.port)
+    https.use_ssl = true
+    ​
+    request = Net::HTTP::Post.new(url)
+    request["Content-Type"] = "application/json"
+    request["Authorization"] = "Bearer PvjKWATucakNi5BKtacYJVvcQUcq"
+    ​
+    request.body = {
+      BusinessShortCode: code,
+      Password:
+        "MTc0Mzc5YmZiMjc5ZjlhYTliZGJjZjE1OGU5N2RkNzFhNDY3Y2QyZTBjODkzMDU5YjEwZjc4ZTZiNzJhZGExZWQyYzkxOTIwMjIxMjIxMTMxMjQy",
+      Timestamp: "20221221131242",
+      TransactionType: "CustomerPayBillOnline",
+      Amount: amount,
+      PartyA: phone,
+      PartyB: code,
+      PhoneNumber: phone,
+      CallBackURL: "https://mydomain.com/path",
+      AccountReference: "NewLife",
+      TransactionDesc: "Payment of drugs"
+    }
+    ​
+    response = https.request(request)
+    puts response.read_body
   end
 end
