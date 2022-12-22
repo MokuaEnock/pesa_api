2c788adc02fd2cb97275833ed0480a2b5f9d055b
Enock <enock.mokua@student.moringaschool.com>
2022-12-22 10:41:11 +0300

    add create sum and create difference methods

--- app/models/sum.rb
+++ app/models/sum.rb
@@ -1,6 +1,10 @@
 class Sum < ApplicationRecord
-  before_save :create_sum
+  before_save :create_sum, :create_difference
 
   def create_sum
+    self.sum = (Number.numberA + Number.numberB)
+  end
+  def create_difference
+    self.difference = Number.numberA - Number.numberB
   end
 end
