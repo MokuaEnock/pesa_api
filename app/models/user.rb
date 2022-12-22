80a2e502ec84b151382b023809d73ff5678836c7
Enock <enock.mokua@student.moringaschool.com>
2022-12-22 16:28:05 +0300

    add functionality to pay_mpesa

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,36 @@
 class User < ApplicationRecord
+  require "uri"
+  require "net/http"
+  url = URI("https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest")
+
   before_save :pay_mpesa
 
   def pay_mpesa
-    
+    ​
+    ​
+    https = Net::HTTP.new(url.host, url.port)
+    https.use_ssl = true
+    ​
+    request = Net::HTTP::Post.new(url)
+    request["Content-Type"] = "application/json"
+    request["Authorization"] = "Bearer zUGbrVJZTCGm1dvYnfWO1gPlhTaT"
+    ​
+    request.body = {
+      BusinessShortCode: 174_379,
+      Password:
+        "MTc0Mzc5YmZiMjc5ZjlhYTliZGJjZjE1OGU5N2RkNzFhNDY3Y2QyZTBjODkzMDU5YjEwZjc4ZTZiNzJhZGExZWQyYzkxOTIwMjIxMjIyMTYyMDI3",
+      Timestamp: "20221222162027",
+      TransactionType: "CustomerPayBillOnline",
+      Amount: 1,
+      PartyA: 254_708_374_149,
+      PartyB: 174_379,
+      PhoneNumber: 254_708_374_149,
+      CallBackURL: "https://mydomain.com/path",
+      AccountReference: "CompanyXLTD",
+      TransactionDesc: "Payment of X"
+    }
+    ​
+    response = https.request(request)
+    puts response.read_body
   end
 end
