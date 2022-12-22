b1a6eefccc06633b74eca0822ef82341ea5c5757
Enock <enock.mokua@student.moringaschool.com>
2022-12-22 14:20:39 +0300

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_22_070405) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_22_111435) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_22_070405) do
     t.integer "numberB"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "sum"
   end
 
   create_table "sums", force: :cascade do |t|
