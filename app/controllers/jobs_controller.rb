9d737edc32a809b5479d8691f5d04a5712a26f02
Enock <enock.mokua@student.moringaschool.com>
2022-12-25 18:31:37 +0300

    add fetch data method in jobs controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,16 @@
 class JobsController < ApplicationController
+  def fetch
+    # require "net/http"
+    url =
+      URI(
+        "https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=d0291057&app_key=e45310af6518f33ea0f2617638ff1d7f"
+      )
+    http = Net::HTTP.new(url.host, url.port)
+
+    request = Net::HTTP::GET.new(url)
+
+    response = http.request(request)
+
+    render  json: response.body
+  end
 end
