76ded8e6e76f9d4fad17de66f5dfb2609139cc4e
Enock <enock.mokua@student.moringaschool.com>
2023-01-03 12:05:37 +0300

    add mpesa index method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,40 @@ class UsersController < ApplicationController
     render json: user
   end
 
+  def mpesa_index
+    token = get_access_token
+
+    url = URI("https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest")
+
+    https = Net::HTTP.new(url.host, url.port)
+    https.use_ssl = true
+
+    request = Net::HTTP::Post.new(url)
+    request["Authorization"] = "Bearer #{token}"
+    request["Content-Type"] = "application/json"
+    password =
+      Base64.strict_encode64(
+        "174379bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919#{Time.now.strftime "%Y%m%d%H%M%S"}"
+      )
+
+    request.body = {
+      BusinessShortCode: 174_379,
+      Password: password,
+      Timestamp: "#{Time.now.strftime "%Y%m%d%H%M%S"}",
+      TransactionType: "CustomerPayBillOnline",
+      Amount: 100,
+      PartyA: params[:phone_number],
+      PartyB: 174_379,
+      PhoneNumber: params[:phone_number],
+      CallBackURL: "https://hired-app-api.herokuapp.com/mpesa_callback",
+      AccountReference: "Hired LTD",
+      TransactionDesc: "Payment of subscription"
+    }.to_json
+
+    response = https.request(request)
+    render json: response.body
+  end
+
   private
 
   def user_params
@@ -50,5 +84,4 @@ class UsersController < ApplicationController
     response = https.request(request)
     puts response.read_body
   end
-
 end
